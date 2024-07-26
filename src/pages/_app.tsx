ea5a39fc787dec8aa24465f8357793d12825e80f
ankushKun <ankush4singh@gmail.com>
2024-07-26 12:44:10 +0530

    deployment cards

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ThemeProvider } from "@/components/theme-provider"
+import { ArweaveWalletKit } from "arweave-wallet-kit";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <ThemeProvider
+    defaultTheme="dark"
+    attribute="class"
+  ><ArweaveWalletKit config={{
+    permissions: ["ACCESS_ADDRESS", "SIGN_TRANSACTION"],
+    ensurePermissions: true,
+  }}>
+      <Component {...pageProps} />
+    </ArweaveWalletKit>
+  </ThemeProvider>
 }
---
+++ src/pages/deploy.tsx
@@ -0,0 +1,7 @@
+export default function Deploy() {
+    return (
+        <div>
+            <h1>Deploy</h1>
+        </div>
+    );
+}
